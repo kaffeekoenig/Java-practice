198f9cd82189666f542f510c60e27c60e14fa55b
kaffeekoenig <coffeeking@list.ru>
2024-10-14 16:59:16 +0300

    VirtualUser logic fixed

--- amusementPark/VirtualUser.java
+++ amusementPark/VirtualUser.java
@@ -3,24 +3,43 @@ package amusementPark;
 import java.util.Random;
 
 public class VirtualUser extends User{
-    int wallet;
+    private int wallet;
     public VirtualUser(int money){
         wallet = money;
     }
+
+    private static Attraction findMin(Attraction[] attractions) {
+        Attraction minPricedAttr = null;
+        int minPrice = Integer.MAX_VALUE;
+        for (Attraction a : attractions) {
+            if (a.getPrice() < minPrice) {
+                minPrice = a.getPrice();
+                minPricedAttr = a;
+            }
+        }
+        return minPricedAttr;
+    }
+
     @Override
-    void makeFun() {
+    public void makeFun() {
         Random random = new Random();
         int atrNum = random.nextInt(3);
-        Attraction[] attractions = {new RollyCoaster(), new Carousel(), new FuturamaCab()};
-        int minPrice = Integer.MAX_VALUE;
+        Attraction[] attractions = {new RollerCoaster(), new Carousel(), new FuturamaCab()};
+        Attraction minPricedAttr = findMin(attractions);
+        int attrPrice = 0;
 
-        for (Attraction a : attractions) {
-            minPrice = Math.min(minPrice, a.getPrice());
-        }
-        
-        while (wallet >= minPrice) {
-            attractions[atrNum].ride();
-            wallet -= attractions[atrNum].getPrice();
+        while (wallet > minPricedAttr.getPrice()) {
+            attrPrice = attractions[atrNum].getPrice();
+
+            if (wallet > attrPrice) {
+                attractions[atrNum].ride();
+                wallet -= attrPrice;
+            } else if (wallet > minPricedAttr.getPrice()) {
+                minPricedAttr.ride();
+                wallet -= minPricedAttr.getPrice();
+            } else if (attractions[2].getPrice() == 0) {
+                attractions[2].ride();
+            }
             atrNum = random.nextInt(3);
         }
         System.out.println("out of money :(");
