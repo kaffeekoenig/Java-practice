f1e136a58047012428bb6a3a854042eeee2d75e1
kaffeekoenig <coffeeking@list.ru>
2024-10-14 16:51:12 +0300

    small encapsulation fixes & update visit() with new algorithm

--- amusementPark/FuturamaCab.java
+++ amusementPark/FuturamaCab.java
@@ -1,18 +1,22 @@
 package amusementPark;
 
 public class FuturamaCab extends Attraction {
-    int count = 0;
+    private int count = 0;
+    private static final int REGULAR_PRICE = 5;
     public FuturamaCab(){
-        price = 500;
-        placeCount = 1;
+        setPrice(REGULAR_PRICE);
     }
 
-    void visit() {
+    private void visit() {
         count++;
-        price++;
+        if (count == 100) {
+            setPrice(0);
+            System.out.println("u are the hundredth visitor!\nu can use Futurama Cab for free once!");
+        }
+        else setPrice(REGULAR_PRICE);
     }
     @Override
-    void ride() {
+    public void ride() {
         visit();
         System.out.println("u tried to die already " + count + " times lol");
     }
