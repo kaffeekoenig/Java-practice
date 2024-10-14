1c14b30c2c09a957e013756ecf3ec3e6ef309d00
kaffeekoenig <coffeeking@list.ru>
2024-10-14 16:41:03 +0300

    small fixes & add error output

--- amusementPark/Main.java
+++ amusementPark/Main.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0)
-        {
-            return;
-        }
-        String type = args[0];
+        if (args.length == 0) return;
 
+        String type = args[0];
         User user;
+
         switch (type) {
             case "-v" -> {
-                if (args.length >= 2) {
+                if (args.length == 2) {
                     Scanner s = new Scanner(args[1]);
                     if (s.hasNextInt()) {
                         user = new VirtualUser(s.nextInt());
                         user.makeFun();
-                    }
-                }
+                    } else System.err.println("user has no money");
+                } else System.err.println("invalid arguments");
             }
             case "-u" -> {
                 user = new RealUser();
