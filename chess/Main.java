1cb87beb1c39adfdef7dcd04e3e5ceb02c94091c
kaffeekoenig <coffeeking@list.ru>
2024-10-15 18:28:30 +0300

    add output

--- chess/Main.java
+++ chess/Main.java
@@ -1,10 +1,27 @@
 package chess;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.logging.Logger;
+
 public class Main {
+    public static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-        String filename = "masha-sasha.txt";
-        String[] players = filename.split("-");
+        String fileName = "masha-sasha.txt";
+        String path = String.valueOf(Main.class.getResource(fileName)).substring(5);
+
+        String[] players = fileName.split("-");
         players[1] = players[1].substring(0, players[1].indexOf("."));
         System.out.println("Игрок 1: " + players[0] + " - " + "Игрок 2: " + players[1]);
+
+        try (FileReader fileReader = new FileReader(path)) {
+            String s = "";
+            while (fileReader.ready()) System.out.print((char)fileReader.read());
+        } catch (IOException e) {
+            logger.severe("something went wrong >> " + e);
+        }
     }
 }
