a05f3ce9959d48c93c7cd289865580afc7ff1489
kaffeekoenig <coffeeking@list.ru>
2024-10-17 15:22:57 +0300

    new OOP practice initial commit

---
+++ met/MethodHolder.java
@@ -0,0 +1,16 @@
+package met;
+
+public class MethodHolder {
+    public String met1(){
+        return "public method!";
+    }
+    private String met2(){
+        return "private method!";
+    }
+    protected String met3(){
+        return "pritected method!";
+    }
+    String met4(){
+        return "method!";
+    }
+}
