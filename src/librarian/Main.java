af9b92b3d101c7b271284a9fc533e03d7f3eec86
kaffeekoenig <coffeeking@list.ru>
2025-04-18 12:35:06 +0300

    added main for librarian

---
+++ src/librarian/Main.java
@@ -0,0 +1,8 @@
+package librarian;
+
+public class Main {
+    public static void main(String[] args) {
+        Librarian myLibrarian = new Librarian("D:\\programming\\prac\\src\\books.txt");
+        myLibrarian.start();
+    }
+}
